e8f485d3f06014083193b457a45ef6024ec46a24
Daniel Belasco Rogers <danbelasco@yahoo.co.uk>
2015-07-25 23:08:08 +0200

    Improved docstring in readme and script

--- preprocessGPXtracks.py
+++ preprocessGPXtracks.py
@@ -1,27 +1,38 @@
 #!/usr/bin/env python
 """
-A script that takes a gpx file and copies it, writing a new track
-around every segment, changing the track name of each track to the
-date time of the first trackpoint, and deleting tracks with 3
-trackpoints or less (see minpoints option). It also ignores empty
-track segments in the original file, removes duplicate segments
-(based on the first timestamp occuring more than once), and sorts
-the track segments by time.
+A script that takes a gpx file, saving a new one with a standard
+suffix '_pp' (that can be changed with the --suffix option) after
+performing the following adjustments to it:
+1. Each segment in the original file becomes a track in the new
+   file i.e. tracks and tracksegments become de facto the same
+   thing.
+2. Each new track has the name of the timestamp of the first
+   trackpoint.
+3. Tracksegments/Tracks with less than the default number of 3
+   trackpoints are deleted from the processed file. This can be
+   changed with the --minpoints option.
+4. Empty track segments in the original file are deleted
+5. Duplicate segments (based on the first timestamp occuring more
+   than once) are deleted.
+6. Track segments are sorted by time.
+7. Trackpoints with a missing 'ele' (elevation) tag get added with
+   a value of 0.
+8. The --crop option drops the first and last point from every
+   trackseg (this improves cleaning as these points are often
+   spurious).
 
-With the --crop option, it can also drop each first and last point
-from every trackseg (this improves cleaning as these points are
-often spurious).
-
-Takes one argument, the path to the file to process
+There is one argument: the path to the gpx file to process
 
 Options:
 * --suffix <string>: change the file suffix
-* --destination <string>: specify a destination folder for the processed file
+* --destination <string>: specify a destination folder for the
+    processed file
 * --crop: turn on cropping of first and last trackpoints
-* --minpoints <integer>: set the maximum number of points a track must have
-* --quiet: don't print information to STDOUT
-* --stdout: print new file to STDOUT for redirecting. Sets quiet mode to True
-* See --help for details
+* --minpoints <integer>: set the maximum number of points a track
+    must have
+* --quiet: don't print any information to STDOUT
+
+See --help for details
 
 I call it preprocesssing because I have several processes to go
 through before the file is accepted into our spatialite reference
@@ -30,7 +41,7 @@ database. So this is a preliminary step before running
 
 Personal note: an adaptation of renameGPXtracksEtree.py
 
-Copyright 2011 Daniel Belasco Rogers <http://planbperformance.net/dan>
+Copyright 2011 Daniel Belasco Rogers dbr <danbelasco@yahoo.co.uk>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
